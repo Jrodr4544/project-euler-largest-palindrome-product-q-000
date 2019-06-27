f890152f0b4639cfc5d58f660b2747c89b16c9d8
jrodr4544 <maskdbandit@gmail.com>
2019-06-27 14:11:40 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+    range = Array(100..999)
+    products = []
+    palindromous = []
+
+    range.each do |element|
+        products.push(range.map{|x| x * element })
+    end
+
+    products.flatten.each do |element|
+        palindromous.push(element) if element.to_s == element.to_s.reverse
+    end
+    
+    return palindromous.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    
+    def initialize
+    end
+
+    def answer
+        range = Array(100..999)
+        products = []
+        palindromous = []
+
+        range.each do |element|
+            products.push(range.map{|x| x * element })
+        end
+
+        products.flatten.each do |element|
+            palindromous.push(element) if element.to_s == element.to_s.reverse
+        end
+    
+        return palindromous.max
+    end
+end
